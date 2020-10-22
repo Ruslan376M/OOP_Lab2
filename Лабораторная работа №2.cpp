078dbad8e1aaf082c3a5cf0066e5a8ea16d0c87b
Руслан Вальшин <Ruslan376@outlook.com>
2020-10-23 01:04:02 +0500

    Реализован базовый класс CelestialBody

--- Лабораторная работа №2.cpp
+++ Лабораторная работа №2.cpp
@@ -1,20 +1,40 @@
-﻿// Лабораторная работа №2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
 
-#include <iostream>
+class CelestialBody
+{
+public:
+    double mass;// В килограммах
+    double volume; // В кубических километрах
+    
+    CelestialBody()
+    {
+        mass = 0;
+        volume = 0;
+        printf("CelestialBody()\n");
+    }
+
+    CelestialBody(double mass, double volume)
+    {
+        this->mass = mass;
+        this->volume = volume;
+        printf("CelestialBody(double mass, double volume)\n");
+    }
+
+    CelestialBody(const CelestialBody &body)
+    {
+        mass = body.mass;
+        volume = body.volume;
+        printf("CelestialBody(const CelestialBody &body)");
+    }
+
+    ~CelestialBody()
+    {
+        printf("mass = %f, volume = %f\n", mass, volume);
+        printf("~CelestialBody()\n");
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
